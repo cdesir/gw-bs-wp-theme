f49fb45101e6b4448a2f919e8cef214f1db10ebc
Cdesir <cliffdesir@gmail.com>
2020-05-24 14:19:50 -0400

    added bootstrap to theme

--- header.php
+++ header.php
@@ -1,1 +1,13 @@
-THIS IS THE HEADER
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body>
