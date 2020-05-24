a83e88d711ab1d4fdcef63dbc6661616a5759c2a
Cdesir <cliffdesir@gmail.com>
2020-05-24 19:07:00 -0400

    added bootstrap menu

--- functions.php
+++ functions.php
@@ -1,16 +1,20 @@
 <?php
 
+// Load style sheets
 function load_css()
 {
 
-    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all');
+    wp_enqueue_style('main');
 
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
     wp_enqueue_style('bootstrap');
 }
 
 add_action('wp_enqueue_scripts', 'load_css');
 
 
+// Load Javascript
 function load_js()
 {
 
@@ -22,3 +26,18 @@ function load_js()
 }
 
 add_action('wp_enqueue_scripts', 'load_js');
+
+
+// Theme Options
+add_theme_support('menus');
+
+// Register Custom Navigation Walker
+require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+
+
+// Menus
+register_nav_menus(
+    array(
+        'primary' => 'Primary Menu',
+    )
+);
